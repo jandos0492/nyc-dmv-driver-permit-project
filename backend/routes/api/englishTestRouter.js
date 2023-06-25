ce9879956cd33da055aae0cb309781f29434db1a
jandos0492 <jandos0492@gmail.com>
2023-06-25 05:32:30 -0400

    Added routes to get the question by id in RussianTest and EnglishTest

--- backend/routes/api/englishTestRouter.js
+++ backend/routes/api/englishTestRouter.js
@@ -9,6 +9,21 @@ router.get("/english", async (req, res) => {
     console.error(err);
     res.status(500).json({ message: "Server Error" });
   }
-})
+});
+
+router.get("/english/:id", async (req, res) => {
+  const questionId = req.params.id;
+  try {
+    const question = await EnglishTest.findByPk(questionId);
+
+    if (!question) {
+      return res.status(404).json({ message: "Question not found" });
+    }
+    res.json(question);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+ });
 
 module.exports = router;
--- backend/routes/api/russianTestRouter.js
+++ backend/routes/api/russianTestRouter.js
@@ -9,6 +9,22 @@ router.get("/russian", async (req, res) => {
     console.error(err);
     res.status(500).json({ message: "Server Error" });
   }
-})
+});
+
+router.get("/russian/:id", async (req, res) => {
+  const questionId = req.params.id;
+  try {
+    const question = await RussianTest.findByPk(questionId);
+
+    if (!question) {
+      return res.status(404).json({ message: "Question not found"});
+    }
+
+    res.json(question);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
 
 module.exports = router;
