2a85e36fd801ef6b87ec9ae23bcc8b552ecf05cb
jandos0492 <jandos0492@gmail.com>
2023-07-01 04:09:57 -0400

    Added the card-description for the Home page

--- frontend/src/components/EnglishHomePage/index.js
+++ frontend/src/components/EnglishHomePage/index.js
@@ -1,26 +1,35 @@
-import React, { useEffect, useState } from "react";
-import { Link, Routes, Route } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 import LearnPage from "../LearnPage";
 import "./EnglishHomePage.css";
 
 const EnglishHomePage = ({ language }) => {
-
   return (
     <>
-      {language === 'english' && (
+      {language === "english" && (
         <div className="home">
           <Link to="/learn" className="home-card green">
             <span>Learn</span>
+            <p className="card-description">
+              Learn all the questions and prepare for the NYC permit test
+            </p>
           </Link>
           <Link to="/full-test" className="home-card yellow">
             <span>Full Test</span>
+            <p className="card-description">
+              Pass all the questions test and get the result
+            </p>
           </Link>
           <Link to="/express-test" className="home-card red">
             <span>Express Test</span>
+            <p className="card-description">
+              Pass only 20 random questions, like the real test in NYC DMV
+            </p>
           </Link>
         </div>
       )}
     </>
   );
 };
+
 export default EnglishHomePage;
--- frontend/src/components/RussianHomePage/index.js
+++ frontend/src/components/RussianHomePage/index.js
@@ -10,13 +10,22 @@ const RussianHomePage = ({ language }) => {
       {language === 'russian' && russianData && (
         <div className="home">
           <Link to="/ru/learn" className="home-card green">
-           <span>Подготовка</span> 
+           <span>Подготовка</span>
+           <p className="card-description">
+            Изучите все вопросы и подготовтесь к тесту на получение водительского пермита в Нью-Йорке
+           </p>
           </Link>
           <Link to="/ru/full-test" className="home-card yellow">
             <span>Полный Тест</span>
+            <p className="card-description">
+              Пройдите тест по всем вопросам и получите результат
+            </p>
           </Link>
           <Link to="/ru/express-test" className="home-card red">
             <span>Быстрый Тест</span>
+            <p className="card-description">
+              Пройдите тест по 20 случайным вопросам, как в настоящем тесте
+            </p>
           </Link>
         </div>
       )}
